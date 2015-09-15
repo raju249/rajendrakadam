dcf1c604471d93bd03a555275e162d7a41d228e4
Rajendra <rajendrakadam249@gmail.com>
2015-09-15 23:02:33 +0530

    Added Port from OS

--- rajendra.py
+++ rajendra.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask,render_template,url_for,redirect
 
 #configuring app
 app = Flask(__name__)
 
+port = iint(os.environ.get("PORT",5000))
 #routing view function
 @app.route('/')
 @app.route('/index')
@@ -17,4 +19,4 @@ def codesnippets():
 if __name__ == "__main__":
         app.debug = False
         app.use_reloader=True
-        app.run()
+        app.run(host='0.0.0.0',port = port)
